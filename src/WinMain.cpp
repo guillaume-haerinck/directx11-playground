39520e7e7a1f6ee30d4e726a94c2ebbdb9fbb0df
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-08-14 21:37:31 +0200

    create window

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -6,5 +6,34 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow
 ) {
+	const auto pClassName = "hwd3dPlayground";
+
+	// Register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// Create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName, "DirectX 11 Playground",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480, nullptr, nullptr,
+		hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+	while (true);
+
 	return 0;
 }
