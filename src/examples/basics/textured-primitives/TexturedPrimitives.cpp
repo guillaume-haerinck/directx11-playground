e9c790a2ca1795ec1f4700e3073dde18ef585759
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-09-20 00:44:09 +0200

    Set textured primitives example

--- src/examples/basics/textured-primitives/TexturedPrimitives.cpp
+++ src/examples/basics/textured-primitives/TexturedPrimitives.cpp
@@ -7,7 +7,9 @@
 #include "systems/CameraSystem.h"
 #include "graphics/ConstantBuffer.h"
 #include "components/physics/Transform.h"
+#include "components/graphics/Pipeline.h"
 #include "scomponents/graphics/ConstantBuffers.h"
+#include "scomponents/graphics/Shaders.h"
 
 namespace basicExample {
 	TexturedPrimitives::TexturedPrimitives(Context& context) : m_ctx(context) {
@@ -18,33 +20,46 @@ namespace basicExample {
 			std::make_shared<RenderSystem>(context)
 		};
 
-		// Get constant buffers
+		// Get singleton components
 		auto graphEntity = m_ctx.singletonComponents.at(scomp::SingletonEntities::SING_ENTITY_GRAPHIC);
 		scomp::ConstantBuffers& cbs = m_ctx.registry.get<scomp::ConstantBuffers>(graphEntity);
+		scomp::Shaders& shaders = m_ctx.registry.get<scomp::Shaders>(graphEntity);
+		scomp::PhongMaterials& materials = m_ctx.registry.get<scomp::PhongMaterials>(graphEntity);
 
 		// Vertex shader
 		scomp::VertexShader VShader = m_ctx.rcommand->CreateVertexShader(primFactory.GetIed(), primFactory.GetIedElementCount(), L"res/built-shaders/TexturedPrimitives_VS.cso");
 		VShader.constantBuffers.push_back(cbs.constantBuffers.at(scomp::ConstantBufferIndex::PER_MESH).buffer);
 		VShader.constantBuffers.push_back(cbs.constantBuffers.at(scomp::ConstantBufferIndex::PER_FRAME).buffer);
+		shaders.vss.push_back(VShader);
 
 		// Pixel Shader
 		scomp::PixelShader PShader = m_ctx.rcommand->CreatePixelShader(L"res/built-shaders/TexturedPrimitives_PS.cso");
+		shaders.pss.push_back(PShader);
 		
+		// Material
+		scomp::PhongMaterial material = {};
+		scomp::Texture texture = m_ctx.rcommand->CreateTexture(0, L"res/textures/test.jpg");
+		material.textures.push_back(texture);
+		materials.materials.push_back(material);
+
 		// Mesh
 		comp::Mesh mesh = primFactory.CreateIcosahedron();
-		mesh.materialIndex = 0;
-		scomp::Texture texture = m_ctx.rcommand->CreateTexture(0, L"res/textures/test.jpg");
+		mesh.materialIndex = materials.materials.size() - 1;
+		mesh.materialType = scomp::MaterialType::PHONG;
 
-		// TODO FIXME
-		// mesh.textures.push_back(texture.srv);
+		// Pipeline
+		comp::Pipeline pipeline = {};
+		pipeline.hasShader.at(comp::PipelineShaderIndex::PS) = true;
+		pipeline.hasShader.at(comp::PipelineShaderIndex::VS) = true;
+		pipeline.vsIndex = shaders.vss.size() - 1;
+		pipeline.psIndex = shaders.pss.size() - 1;
 
 		// Transformm
 		comp::Transform transform = {};
 
 		// Assign data to an entity
 		auto entity = m_ctx.registry.create();
-		m_ctx.registry.assign<scomp::VertexShader>(entity, VShader);
-		m_ctx.registry.assign<scomp::PixelShader>(entity, PShader);
+		m_ctx.registry.assign<comp::Pipeline>(entity, pipeline);
 		m_ctx.registry.assign<comp::Mesh>(entity, mesh);
 		m_ctx.registry.assign<comp::Transform>(entity, transform);
 	}
